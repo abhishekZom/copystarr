53d6cd439a954607af11efade94e3b5c20bd6400
Abhishek Kumar <abhi28k09@gmail.com>
2023-06-28 17:30:44 +0530

    new pics

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ export default Header;
 
 const HeaderWrapper = styled.div`
   z-index: 99999;
-  height: 10vh;
+  height: 12vh;
   background: #141F29;
   color: white;
   position: fixed;
@@ -18,8 +18,8 @@ const HeaderWrapper = styled.div`
   /* opacity: 0.9; */
   font-size: 2.2rem;
   display: flex;
+  justify-content: center;
   align-items: center;
-  padding-left: 2rem;
   -webkit-box-shadow: -10px 7px 16px -6px rgba(0,0,0,0.39);
   -moz-box-shadow: -10px 7px 16px -6px rgba(0,0,0,0.39);
   box-shadow: -10px 7px 16px -6px rgba(0,0,0,0.39);
